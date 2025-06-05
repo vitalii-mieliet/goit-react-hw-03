9ecc804fcf73162e869350ceb328612f14c06c51
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-05 15:44:35 +0300

    chore: add basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 function App() {
   return (
-    <>
-      <h1>Phone Book</h1>
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   );
 }
 
