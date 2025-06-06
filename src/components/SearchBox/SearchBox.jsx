a3194591dd5db7c234eb36468b22ccb0c1479f8a
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-06 12:46:31 +0300

    style: add style to serch box component

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,11 +1,22 @@
+import { useId } from "react";
+import css from "./SearchBox.module.css";
+
 const SearchBox = ({ filterValue, handleFilter }) => {
+  const id = useId();
   const handleChange = (e) => {
     handleFilter(e.target.value);
   };
 
   return (
-    <div>
-      <input value={filterValue} type="text" onChange={handleChange} />
+    <div className={css.container}>
+      <label htmlFor={id}>Find contacts by name</label>
+      <input
+        className={css.input}
+        id={id}
+        value={filterValue}
+        type="text"
+        onChange={handleChange}
+      />
     </div>
   );
 };
